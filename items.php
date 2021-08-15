14fff7cb58cfcde3966da1ceb280ff05013f6a09
swayamgupta74 <69929076+swayamgupta74@users.noreply.github.com>
2021-08-15 12:24:19 +0530

    correcting file paths

--- items.php
+++ items.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="c_dashboard.css">
-    <link rel="stylesheet" href="c_orders.css">
+    <link rel="stylesheet" href="customer/c_dashboard.css">
+    <link rel="stylesheet" href="customer/c_orders.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
